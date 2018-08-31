b3191a1dc93097823e9faab74103250fa846b512
Amarico <Amarico@equipo-zalman>
2018-08-31 23:19:25 +0200

    // Importo la clase java.util.* para utilizar Scanner
    import java.util.*;

---
+++ src/ejercicio6adaboletin3/Ejercicio6AdaBoletin3.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio6adaboletin3;
+
+
+/**
+ *
+ * @author Amarico
+ */
+public class Ejercicio6AdaBoletin3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // Realizar un programa que nos pida un número n, 
+        // y nos diga cuantos números hay entre 1 y n que son primos. 
+        
+        
+    }
+    
+}
